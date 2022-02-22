41875eb1853e0c8b54b5282e497c4b32ee6fae5e
Sreenath285 <sreenath.285@gmail.com>
2022-02-22 12:51:39 +0530

    computing employee wage using class method and variables

--- EmpWageOOP/src/Main.java
+++ EmpWageOOP/src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
 
+    /*
+        Program to compute employee wage
+     */
+
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program");
+
+        System.out.println("Welcome to Employee Wage Computation Program\n");
+
+        Employee actEmp = new Employee("ACT", 10, 10, 50);
+        actEmp.calculateEmpWage();
+
+        Employee trendsEmp = new Employee("Trends", 20, 20, 100);
+        trendsEmp.calculateEmpWage();
+
     }
 }
